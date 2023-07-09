b2ce4c0747c5d5685860d881b6a175bce9094ab7
MBoota <m43577535@gmail.com>
2023-07-09 07:08:04 -0700

    continue

--- routing-Controller-UITemplate-Layout-Errors/app/Http/Controllers/FormSubmitionController.php
+++ routing-Controller-UITemplate-Layout-Errors/app/Http/Controllers/FormSubmitionController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
+
 
 class FormSubmitionController extends Controller
 {
@@ -43,11 +46,87 @@ class FormSubmitionController extends Controller
     }
     
     public function FormAllControllesPost(Request $req){
+        //________________________ 1. Validation _____________________
         $req->validate([
-            'userName'=>'required',
-            'fName'=> 'required',
-            
-        ]);
-        return View("dashboard.formSubmit.postData");
+            'userName' => 'required|max:20|min:3',
+            'fatherName' => 'required',
+            'email' => 'required|max:20|min:3|email',
+            'age' => 'required',
+           // 'Gender' => 'required',
+            'country' => 'required',
+            'Subject' => 'required',
+            'password' => 'required|min:8',
+            'conPassword' => 'required|same:password',
+        ], [
+            'userName.required' => 'The user name field is required.',
+            'userName.max' => 'The user name may not be greater than :max characters.',
+            'userName.min' => 'The user name must be at least :min characters.',
+
+            'fatherName.required' => 'The father name field is required.',
+
+            'email.required' => 'The email field is required.',
+            'email.max' => 'The email may not be greater than :max characters.',
+            'email.min' => 'The email must be at least :min characters.',
+            'email.email' => 'Please enter a valid email address.',
+
+            'age.required' => 'The age field is required.',
+
+            //'Gender.required' => 'The gender field is required.',
+
+            'country.required' => 'The country field is required.',
+
+            'Subject.required' => 'The subject field is required.',
+
+            'password.required' => 'The password field is required.',
+            'password.min' => 'The password must be at least :min characters.',
+
+            'conPassword.required' => 'The confirm password field is required.',
+            'conPassword.same' => 'The confirm password and password must match.',
+        ]);        
+
+        //________________________ 2. Shwoing for  Understanding ________________
+        echo $req->UserName . "<br>";
+        echo $req->fatherName . "<br>";
+        echo $req->age . "<br>";
+        echo $req->email. "<br>";
+        echo $req->Gender . "<br>";
+        echo $req->country . "<br>";
+        print_r($req->Subject)  . "<br>";
+
+        //_________ basice password _________
+        // echo $req->password . "<br>";
+        // echo $req->conPassword . "<br>";
+
+        //_________ md5 password _________
+        echo md5($req->password) . "<br>";
+        echo md5($req->conPassword) . "<br><br>";
+        //we can not get real password
+
+        //_________ encr, dec password _________
+        $encryptString = Crypt::encryptString($req->password);
+        echo "EncryptString : " . $encryptString ."<br>";
+
+        $deCryptString = Crypt::decryptString($encryptString);
+        echo "DeCryptString : " . $deCryptString ."<br>";
+
+
+        //_________ has password _________
+        //For secure password hashing in Laravel, it is recommended to use the built-in Hash facade 
+        $hashedPassword = Hash::make($req->password);
+        echo "Has Password " . $hashedPassword . "</br>";
+        
+        //==== to check  has with database ====
+        // $user = User::where('email', $req->email)->first(); // Assuming 'User' is your user model
+        // if ($user && Hash::check($req->password, $user->password)) {
+        //     // Passwords match, proceed with authentication
+        // } else {
+        //     // Invalid credentials
+        // }
+
+        //_________________________ Image Uploading ________________
+        echo $req->img . "<br>";
+
+        //________________________ 3. After Clearing all Concepts //________________________ 
+        // return View("dashboard.formSubmit.postData");
     }
 }
--- routing-Controller-UITemplate-Layout-Errors/resources/views/dashboard/formSubmit/FormAllControlles.blade.php
+++ routing-Controller-UITemplate-Layout-Errors/resources/views/dashboard/formSubmit/FormAllControlles.blade.php
@@ -2,77 +2,129 @@
 
 @section("content")
 
-    <h1>Form All Controll</h1>
+    <h2>Form All Controll</h2>
     <br>
 
-    <form method="POST" action="{{url('/class3/_3_FormAllControllesPost')}}">
+    <form method="POST" action="{{url('/class3/_3_FormAllControllesPost')}}" enctype="multipart/form-data">
         @csrf
-        <!--____________ 1. Usernmae , fName , age __________-->
+      <!--____________ 1. Usernmae , fName , age __________-->
         <div class="form-group row">
-            <label for="exampleInputUsername2" class="col-sm-2 col-form-label">UserName</label>
-            <div class="col-sm-9">
-              <input type="text" class="form-control" id="exampleInputUsername2" placeholder="Username" name="userName">
-            </div>
+          <label for="exampleInputUsername2" class="col-sm-2 col-form-label">UserName</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" value="{{old('userName')}}" id="exampleInputUsername2" placeholder="Username" name="userName">
+            @error('userName')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
           </div>
-      
-          <div class="form-group row">
-            <label for="exampleInputEmail2" class="col-sm-2 col-form-label">FatherName</label>
-            <div class="col-sm-9">
-              <input type="email" class="form-control" id="exampleInputEmail2" placeholder="fName" name="fatherName">
-            </div>
+          
+        </div>
+    
+        <div class="form-group row">
+          <label for="exampleInputEmail2" class="col-sm-2 col-form-label">FatherName</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" value="{{old('fatherName')}}" id="exampleInputEmail2" placeholder="fName" name="fatherName">
+            @error('fatherName')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
           </div>
-      
-          <div class="form-group row">
-            <label for="exampleInputMobile" class="col-sm-2 col-form-label">Age</label>
-            <div class="col-sm-9">
-              <input type="text" class="form-control" id="exampleInputMobile" placeholder="Age" name="age">
-            </div>
-          </div>
+        </div>
 
-          <!--____________ 2. CheckBox __________-->
-          <div class="form-group row">
-            <label for="maleGender" class="col-sm-2 col-form-label">Gender</label>
-            <div class="col-sm-9">
-              <label><input type="radio" id="maleGender" placeholder="Age" name="Gender" value="M">&nbsp; Male </label>&nbsp; &nbsp; 
-              <label><input type="radio" id="femaleGender" placeholder="Age" name="Gender" value="F">&nbsp; Female </label>
-            </div>
+        <div class="form-group row">
+          <label for="email1" class="col-sm-2 col-form-label">Email</label>
+          <div class="col-sm-9">
+            <input type="email" class="form-control" value="{{old('email')}}" id="email1" placeholder="email1" name="email">
+            @error('email')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
           </div>
+        </div>
+    
+        <div class="form-group row">
+          <label for="exampleInputMobile" class="col-sm-2 col-form-label">Age</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" value="{{old('age')}}" id="exampleInputMobile" placeholder="Age" name="age">
+            @error('age')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
+          </div>
+        </div>
 
-          <!--____________ 3. Select __________-->
-          <div class="form-group row">
-            <label for="selectInput" class="col-sm-2 col-form-label">Country</label>
-            <div class="col-sm-9">
-              <select id="selectInput" name="country" class="form-control form-select">
-                <option value="" selected>------- Select ----------</option>
+        <!--____________ 2. CheckBox __________-->
+        <div class="form-group row">
+          <label for="maleGender" class="col-sm-2 col-form-label">Gender</label>
+          <div class="col-sm-9">
+            <label><input type="radio" id="maleGender" placeholder="Age" name="Gender" value="M">&nbsp; Male </label>&nbsp; &nbsp; 
+            <label><input type="radio" id="femaleGender" placeholder="Age" name="Gender" value="F">&nbsp; Female </label>
+            @error('Gender')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
+          </div>
+        </div>
+
+        <!--____________ 3. Select __________-->
+        <div class="form-group row">
+          <label for="selectInput" class="col-sm-2 col-form-label">Country</label>
+          <div class="col-sm-9">
+            <select id="selectInput" name="country" class="form-control form-select" value="{{old('country')}}">
+              <option value="" selected>------- Select ----------</option>
                 <option value="Pakistan">Pakistan</option>
-                <option value="India">India</option>
-                <option value="Srilanka">Srilanka</option>
-                <option value="England">England</option>
-                <option value="Australia">Australia</option>
-              </select>
-            </div>
+              <option value="India">India</option>
+              <option value="Srilanka">Srilanka</option>
+              <option value="England">England</option>
+              <option value="Australia">Australia</option>
+            </select>
+            @error('country')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
           </div>
-      
-          <!--____________ 5. checkbox __________-->
-          <div class="form-group row">
-            <label for="sub1" class="col-sm-2 col-form-label">Subjects</label>
-            <div class="col-sm-9 mt-3">
-              <label><input type="checkbox" id="sub1" placeholder="Age" name="Subject[]" value="C#">&nbsp; C# </label>&nbsp; &nbsp; 
-              <label><input type="checkbox" id="sub2" placeholder="Age" name="Subject[]" value="PHP">&nbsp; PHP </label>&nbsp; &nbsp; 
-              <label><input type="checkbox" id="sub3" placeholder="Age" name="Subject[]" value="Laravel">&nbsp; Laravel </label>
-            </div>
+        </div>
+    
+        <!--____________ 5. checkbox __________-->
+        <div class="form-group row">
+          <label for="sub1" class="col-sm-2 col-form-label">Subjects</label>
+          <div class="col-sm-9 mt-3">
+            <label><input type="checkbox" id="sub1" placeholder="Age" name="Subject[]" value="C#">&nbsp; C# </label>&nbsp; &nbsp; 
+            <label><input type="checkbox" id="sub2" placeholder="Age" name="Subject[]" value="PHP">&nbsp; PHP </label>&nbsp; &nbsp; 
+            <label><input type="checkbox" id="sub3" placeholder="Age" name="Subject[]" value="Laravel">&nbsp; Laravel </label>
+            @error('Subject')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
           </div>
+        </div>
 
-          <!--____________ 6. Image __________-->
-          <div class="form-group row">
-            <label for="sub1" class="col-sm-2 col-form-label">Image</label>
-            <div class="col-sm-9">
-              <input type="file" name="img" class="form-control"/>
-            </div>
+        <!--____________ 6. Image __________-->
+        <div class="form-group row">
+          <label for="sub1" class="col-sm-2 col-form-label">Image</label>
+          <div class="col-sm-9">
+            <input type="file" name="img" class="form-control"/>
           </div>
+        </div>
 
-          <button type="submit" class="btn btn-primary me-2">Submit</button>
-          <button class="btn btn-light">Cancel</button>
+        <!--____________ 6. Password , confumr Password __________-->
+        <div class="form-group row">
+          <label for="pass" class="col-sm-2 col-form-label">Password</label>
+          <div class="col-sm-9">
+            <input type="password" class="form-control" id="pass" placeholder="password" name="password">
+            @error('password')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="conPass" class="col-sm-2 col-form-label">Confirm Password</label>
+          <div class="col-sm-9">
+            <input type="password" class="form-control"  id="conPass" placeholder="confirm password" name="conPassword">
+            @error('conPassword')
+              <p class="text-danger">{{$message}}</p>
+            @enderror
+          </div>
+        </div>
+
+
+
+        
+        <button type="submit" class="btn btn-primary me-2">Submit</button>
+        <button class="btn btn-light">Cancel</button>
     </form>    
 
 @endsection
