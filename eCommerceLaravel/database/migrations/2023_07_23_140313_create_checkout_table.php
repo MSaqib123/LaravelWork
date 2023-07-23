42b08298468dfb1caa011014fbf730b78b8541fa
MBoota <m43577535@gmail.com>
2023-07-23 19:10:06 +0500

    1. added deliveryTable 2. added  paymenttable 3. added orderTable 4. added orderDetailTable 5. added checkout table

---
+++ eCommerceLaravel/database/migrations/2023_07_23_140313_create_checkout_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkout', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('mobile');
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('payment_id');
+            $table->unsignedBigInteger('delivery_id');
+            $table->tinyInteger('status')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkout');
+    }
+};
